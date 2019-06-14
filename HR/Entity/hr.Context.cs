d2a7f1105af15dfc4dfc36ffdad1cfd6c12ace2a
YL <837880239@qq.com>
2019-06-14 16:00:28 +0800

    加了对象模型

--- HR/Entity/hr.Context.cs
+++ HR/Entity/hr.Context.cs
@@ -25,5 +25,32 @@ namespace Entity
             throw new UnintentionalCodeFirstException();
         }
     
+        public virtual DbSet<bonus> bonus { get; set; }
+        public virtual DbSet<config_file_first_kind> config_file_first_kind { get; set; }
+        public virtual DbSet<config_file_second_kind> config_file_second_kind { get; set; }
+        public virtual DbSet<config_file_third_kind> config_file_third_kind { get; set; }
+        public virtual DbSet<config_major> config_major { get; set; }
+        public virtual DbSet<config_major_kind> config_major_kind { get; set; }
+        public virtual DbSet<config_primary_key> config_primary_key { get; set; }
+        public virtual DbSet<config_public_char> config_public_char { get; set; }
+        public virtual DbSet<config_question_first_kind> config_question_first_kind { get; set; }
+        public virtual DbSet<config_question_second_kind> config_question_second_kind { get; set; }
+        public virtual DbSet<engage_answer> engage_answer { get; set; }
+        public virtual DbSet<engage_answer_details> engage_answer_details { get; set; }
+        public virtual DbSet<engage_exam> engage_exam { get; set; }
+        public virtual DbSet<engage_exam_details> engage_exam_details { get; set; }
+        public virtual DbSet<engage_interview> engage_interview { get; set; }
+        public virtual DbSet<engage_major_release> engage_major_release { get; set; }
+        public virtual DbSet<engage_resume> engage_resume { get; set; }
+        public virtual DbSet<engage_subjects> engage_subjects { get; set; }
+        public virtual DbSet<human_file> human_file { get; set; }
+        public virtual DbSet<human_file_dig> human_file_dig { get; set; }
+        public virtual DbSet<major_change> major_change { get; set; }
+        public virtual DbSet<salary_grant> salary_grant { get; set; }
+        public virtual DbSet<salary_grant_details> salary_grant_details { get; set; }
+        public virtual DbSet<salary_standard> salary_standard { get; set; }
+        public virtual DbSet<salary_standard_details> salary_standard_details { get; set; }
+        public virtual DbSet<training> training { get; set; }
+        public virtual DbSet<users> users { get; set; }
     }
 }
---
+++ HR/UI/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UI.Filters;
+
+namespace UI.Controllers
+{
+    
+    public class UserController : Controller
+    {
+        
+        // GET: User
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Login]
+        public ActionResult ccc() {
+            return View();
+        }
+        //public ActionResult Error() {
+        //    throw new Exception("报错了！！");
+        //}
+    }
+}
