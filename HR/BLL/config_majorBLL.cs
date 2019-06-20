b13510e7fc7dc26c3b38effb8c156d07f9ebe623
2012314078 <2012314078@qq.com>
2019-06-20 11:35:31 +0800

    人力资源档案管理设置

---
+++ HR/BLL/config_majorBLL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IocContainer;
+using IBLL;
+using IDAL;
+using Entity;
+using System.Linq.Expressions;
+
+namespace BLL
+{
+    class config_majorBLL : Iconfig_majorBLL
+    {
+        Iconfig_majorDao ifm = IocContain.CreateAll<Iconfig_majorDao>("yidao", "config_majorDao");
+        public int Add(config_major t)
+        {
+            return ifm.Add(t);
+        }
+
+        public int Delete(config_major t)
+        {
+            return ifm.Delete(t);
+        }
+
+        public List<config_major> SelectAll()
+        {
+            return ifm.SelectAll<config_major>();
+        }
+
+        public List<config_major> SelectWhere(Expression<Func<config_major, bool>> where)
+        {
+            return ifm.SelectWhere(where);
+        }
+
+        public int Update(config_major t)
+        {
+            return ifm.Update(t);
+        }
+    }
+}
---
+++ HR/IBLL/Iconfig_majorBLL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using System.Linq.Expressions;
+
+namespace IBLL
+{
+    public interface Iconfig_majorBLL
+    {
+        List<config_major> SelectAll();
+
+        List<config_major> SelectWhere(Expression<Func<config_major, bool>> where);
+
+        int Add(config_major t);
+
+        int Delete(config_major t);
+
+        int Update(config_major t);
+    }
+}
