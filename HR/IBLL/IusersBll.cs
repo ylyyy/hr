71291dbaf7f87fe5475fcf081f0c319ca4bace6b
YL <837880239@qq.com>
2019-06-27 14:24:24 +0800

    aa

---
+++ HR/IBLL/IusersBll.cs
@@ -0,0 +1,19 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBLL
+{
+    public interface IusersBll
+    {
+        List<users> SelectAll();
+        List<users> SelectWhere(Expression<Func<users, bool>> where);
+        int Add(users t);
+        int Delete(users t);
+        int Update(users t);
+    }
+}
