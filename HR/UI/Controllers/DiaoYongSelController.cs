b582c1241559f01188c7d3ec35e09d11aaec81b9
zhengqi21 <2835781356@qq.com>
2019-07-06 14:20:46 +0800

    1

---
+++ HR/UI/Controllers/DiaoYongSelController.cs
@@ -0,0 +1,71 @@
+﻿using DAL;
+using Entity;
+using IBLL;
+using IocContainer;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class DiaoYongSelController : Controller
+    {
+        HREntities3 aet = new HREntities3();
+        transferIBLL tib = IocContain.CreateAll<transferIBLL>("yibll", "transferBll");
+        // GET: DiaoYongSel
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SelChange()
+        {
+            Session["yiji"] = Request["firstKindId"];
+            Session["erji"] = Request["configThird.secondKindId"];
+            Session["sanji"] = Request["configThird.thirdKindId"];
+            Session["ksdata"] = Request["utilbean.startDate"];
+            Session["jsdata"] = Request["utilbean.endDate"];
+            return View();
+        }
+        public ActionResult SelChange2()
+        {
+            var yiji = Session["yiji"];
+            var erji = Session["erji"];
+            var sanji = Session["sanji"];
+            var ksdata = Session["ksdata"];
+            var jsdata = Session["jsdata"];
+            string sql = string.Format(@"select * from major_change  where first_kind_id='{0}' or second_kind_id='{1}' or  third_kind_id='{2}' or (regist_time>='{3}' or check_time <='{4}') and check_status=2", yiji, erji, sanji, ksdata, jsdata);
+            DataTable dt = DBHelper.MyDataAdapter(sql, "");
+            return Content(JsonConvert.SerializeObject(dt));
+        }
+        public ActionResult Sel(int id) {
+            var list = tib.SelectWhere(e => e.mch_id == id);
+            major_change mc = new major_change
+            {
+                regist_time=list[0].regist_time,
+                mch_id = list[0].mch_id,
+                human_name = list[0].human_name,
+                first_kind_name = list[0].first_kind_name,
+                second_kind_name = list[0].second_kind_name,
+                third_kind_name = list[0].third_kind_name,
+                major_kind_name = list[0].major_kind_name,
+                major_name = list[0].major_name,
+                salary_standard_name = list[0].salary_standard_name,
+                salary_sum = list[0].salary_sum,
+                register = list[0].register,
+                change_reason = list[0].register,
+                new_salary_sum = list[0].new_salary_sum,
+                new_first_kind_name= list[0].new_first_kind_name,
+                new_second_kind_name= list[0].new_second_kind_name,
+                new_third_kind_name= list[0].new_second_kind_name,
+                new_major_kind_name= list[0].new_major_kind_name,
+                new_major_name= list[0].new_major_name,
+                checker= list[0].checker
+            };
+            return View(mc);
+        }
+    }
+}
---
+++ HR/UI/Controllers/machangeController.cs
@@ -0,0 +1,84 @@
+﻿using DAL;
+using Entity;
+using IBLL;
+using IocContainer;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class machangeController : Controller
+    {
+        HREntities3 aet = new HREntities3();
+        transferIBLL tib = IocContain.CreateAll<transferIBLL>("yibll", "transferBll");
+        // GET: machange
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult index2()
+        {
+            string sql = string.Format(@"select * from major_change where check_status=1");
+            DataTable dt = DBHelper.MyDataAdapter(sql, "");
+            return Content(JsonConvert.SerializeObject(dt));
+        }
+        public ActionResult Edit(int id) {
+            var list = tib.SelectWhere(e => e.mch_id == id);
+            major_change mc = new major_change
+            {
+                mch_id = list[0].mch_id,
+                human_name = list[0].human_name,
+                first_kind_name = list[0].first_kind_name,
+                second_kind_name = list[0].second_kind_name,
+                third_kind_name = list[0].third_kind_name,
+                major_kind_name = list[0].major_kind_name,
+                major_name = list[0].major_name,
+                salary_standard_name = list[0].salary_standard_name,
+                salary_sum = list[0].salary_sum,
+                register = list[0].register,
+                change_reason = list[0].register,
+                new_salary_sum = list[0].new_salary_sum
+            };
+            return View(mc);
+        }
+        public ActionResult Upd()
+        {
+            var id = Request["mch_id"];
+            var status = Request["majorChange.checkStatus"];
+            if (status == "true")
+            {
+                string sql = string.Format(@"update major_change set check_status=2 where mch_id='{0}'",id);
+                int num = DBHelper.InsertUpdateDelte(sql);
+                if (num > 0)
+                {
+                    //return Content("ok");
+                    return Redirect("/machange/hscg");
+                }
+                else {
+                    return View();
+                }
+            }
+            else
+            {
+                string sql = string.Format(@"update major_change set check_status=0 where mch_id='{0}'", id);
+                int num = DBHelper.InsertUpdateDelte(sql);
+                if (num > 0)
+                {
+                    return Content("ok");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+        }
+        public ActionResult hscg() {
+            return View();
+        }
+    }
+}
